b8c3a8573e31d12714ab7566cfdc9fd1a7a22da2
JustKiim <92113747+JustKiim@users.noreply.github.com>
2026-06-23 12:16:13 +0700

    Update coba jonson 4.py

--- coba jonson 4.py
+++ coba jonson 4.py
@@ -1,28 +1,30 @@
+import streamlit as st
 from ortools.sat.python import cp_model
-import collections
 import matplotlib.pyplot as plt
-import pandas as pd
+import collections
 
-def solve_and_visualize():
-    # 1. Input Data
-    try:
-        num_jobs = int(input("Jumlah job: "))
-        num_machines = int(input("Jumlah mesin: "))
-        machine_names = [input(f"Nama Mesin {i+1}: ") for i in range(num_machines)]
-    except ValueError:
-        print("Input harus angka!")
-        return
+st.title("Job Shop Scheduling Solver")
 
-    jobs_data = []
-    for i in range(num_jobs):
-        job_name = input(f"\nNama Job ke-{i+1}: ")
-        job = []
-        for j in range(num_machines):
-            time = int(input(f"  Waktu proses di {machine_names[j]}: "))
-            job.append((j, time))
-        jobs_data.append({'name': job_name, 'tasks': job})
+# 1. Input Data di Streamlit
+num_jobs = st.number_input("Jumlah Job", min_value=1, value=2)
+num_machines = st.number_input("Jumlah Mesin", min_value=1, value=2)
 
-    # 2. Model CP
+machine_names = []
+for i in range(num_machines):
+    machine_names.append(st.text_input(f"Nama Mesin {i+1}", value=f"Mesin {i+1}"))
+
+jobs_data = []
+for i in range(num_jobs):
+    st.subheader(f"Job {i+1}")
+    job_name = st.text_input(f"Nama Job ke-{i+1}", key=f"name_{i}")
+    job = []
+    for j in range(num_machines):
+        time = st.number_input(f"Waktu di {machine_names[j]}", min_value=0, key=f"time_{i}_{j}")
+        job.append((j, int(time)))
+    jobs_data.append({'name': job_name, 'tasks': job})
+
+# 2. Solver (Logic tetap sama)
+if st.button("Hitung Jadwal"):
     model = cp_model.CpModel()
     horizon = sum(task[1] for job in jobs_data for task in job['tasks'])
     all_tasks = {}
@@ -36,8 +38,8 @@ def solve_and_visualize():
             all_tasks[job_id, task_id] = {'start': start, 'end': end, 'dur': duration, 'name': job['name']}
             machine_to_intervals[machine].append(interval)
 
-    for machine in range(num_machines):
-        model.add_no_overlap(machine_to_intervals[machine])
+    for m_idx in range(num_machines):
+        model.add_no_overlap(machine_to_intervals[m_idx])
 
     for job_id, job in enumerate(jobs_data):
         for task_id in range(len(job['tasks']) - 1):
@@ -47,39 +49,21 @@ def solve_and_visualize():
     model.add_max_equality(obj_var, [all_tasks[job_id, len(job['tasks'])-1]['end'] for job_id, job in enumerate(jobs_data)])
     model.minimize(obj_var)
     
-    # 3. Solver & Visualisasi
     solver = cp_model.CpSolver()
     if solver.solve(model) == cp_model.OPTIMAL:
-        print(f"\n--- Makespan Optimal: {solver.objective_value} ---")
+        st.success(f"Makespan Optimal: {solver.objective_value}")
         
-        fig, ax = plt.subplots(figsize=(12, 6))
-        all_times = set()
-        
+        # 3. Visualisasi untuk Streamlit
+        fig, ax = plt.subplots(figsize=(10, 4))
         for m_idx, m_name in enumerate(machine_names):
-            tasks_on_m = []
             for j_id in range(num_jobs):
                 for t_id, (m_id, dur) in enumerate(jobs_data[j_id]['tasks']):
                     if m_id == m_idx:
                         s = solver.value(all_tasks[j_id, t_id]['start'])
                         e = solver.value(all_tasks[j_id, t_id]['end'])
-                        tasks_on_m.append({'start': s, 'end': e, 'name': jobs_data[j_id]['name']})
-                        all_times.update([s, e])
-            
-            tasks_on_m.sort(key=lambda x: x['start'])
-            curr = 0
-            for t in tasks_on_m:
-                if t['start'] > curr:
-                    ax.barh(m_name, t['start'] - curr, left=curr, color='lightgrey', hatch='//', edgecolor='grey')
-                ax.barh(m_name, t['end'] - t['start'], left=t['start'], color='skyblue', edgecolor='black')
-                ax.text(t['start'] + (t['end']-t['start'])/2, m_idx, t['name'], ha='center', va='center')
-                curr = t['end']
+                        ax.barh(m_name, e - s, left=s, edgecolor='black', alpha=0.7)
+                        ax.text(s + (e-s)/2, m_idx, jobs_data[j_id]['name'], ha='center', va='center')
         
-        ax.set_xticks(sorted(list(all_times)))
-        ax.set_title(f"Gantt Chart Optimal (Makespan: {solver.objective_value})")
-        plt.tight_layout()
-        plt.show()
+        st.pyplot(fig)
     else:
-        print("Solusi tidak ditemukan.")
-
-if __name__ == "__main__":
-    solve_and_visualize()
+        st.error("Solusi tidak ditemukan.")
